6f4b15a238f18bf97929d8b4bd1d557375ba3e22
Florent VIOLLEAU <florent.violleau@opera-cesson.fr>
2017-09-02 01:36:42 +0200

    Add script files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Dynhost OVH
+
 Script to update periodically your OVH DNS via DynHost and use the Livebox to retrieve the public IP
 
 ## OVH user creation.
+
 To use that script, you have to set a login and a password. This login and password have to be created by going in the OVH manager. Chose your domain name section, and move to the dyndns tab.
 
 Next move to manage access.
@@ -9,15 +11,17 @@ Next move to manage access.
 ![ovh-1](https://raw.githubusercontent.com/floviolleau/dynhost-ovh-livebox/master/docs/images/ovh-1.png)
 
 Next create an id.
+
 ![ovh-2](https://raw.githubusercontent.com/floviolleau/dynhost-ovh-livebox/master/docs/images/ovh-2.png)
 
-Login use in the script will be: domain-name.[login_you_want]
+Login use in the script will be: domain-name.[login_you_created]
+
 Password use in the script will the one you chose in that screen.
 
 ![ovh-3](https://raw.githubusercontent.com/floviolleau/dynhost-ovh-livebox/master/docs/images/ovh-3.png)
 
-
 ## Cron
+
 Use with cron to launch periodicaly (every two minutes)
 
 ```
---
+++ dynhost.sh
@@ -0,0 +1,70 @@
+#! /bin/sh
+################################################################################
+# Copyright (C) 2017 Florent VIOLLEAU
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT license
+#
+#################################################################################
+# Logfile: dynhost.log
+#
+# CHANGE: « HOST », « LOGIN », « PASSWORD », « LOG_PATH » and « LIVEBOX » to reflect YOUR account variables
+# OR: use, in argument, a custom file that contains « HOST », « LOGIN », « PASSWORD », « LOG_PATH » and « LIVEBOX »
+
+if [ -f "$1" ]; then
+  . $1
+else
+  # HOST can be a domain or a sub-domain
+  HOST='sub-domain.you-domain.fr'
+  # LOGIN is the one you created in ovh manager (see Readme file)
+  LOGIN='you-domain-LOGIN'
+  # PASSWORD is the one you created in ovh manager (see Readme file)
+  PASSWORD='password'
+  LOG_PATH='/var/log/dynhost'
+  LIVEBOX='192.168.1.1'
+fi
+
+# Check binaries
+HAS_CURL=`which curl | echo $?`
+HAS_WGET=`which wget | echo $?`
+HAS_SED=`which sed | echo $?`
+if [ "$HAS_CURL" -eq "1" ]; then
+  echo 'Please install curl' >> $LOG_PATH/dynhost.log
+  exit;
+fi
+
+if [ "$HAS_WGET" -eq "1" ]; then
+  echo 'Please install wget' >> $LOG_PATH/dynhost.log
+  exit;
+fi
+
+if [ "$HAS_SED" -eq "1" ]; then
+  echo 'Please install sed' >> $LOG_PATH/dynhost.log
+  exit;
+fi
+
+echo '----------------------------------' >> $LOG_PATH/dynhost.log
+echo `date` >> $LOG_PATH/dynhost.log
+echo 'DynHost' >> $LOG_PATH/dynhost.log
+
+IP=`curl -s -X POST -H "Content-Type: application/json" -d '{"parameters":{}}'  http://$LIVEBOX/sysbus/NMC:getWANStatus | sed -e 's/.*"IPAddress":"\(.*\)","Remo.*/\1/g'`
+IPv6=`curl -s -X POST -H "Content-Type: application/json" -d '{"parameters":{}}'  http://$LIVEBOX/sysbus/NMC:getWANStatus | sed -e 's/.*"IPv6Address":"\(.*\)","IPv6D.*/\1/g'`
+OLDIP=`dig +short $HOST`
+
+if [ "$IP" ]; then
+  if [ "$OLDIP" != "$IP" ]; then
+    echo -n "Old IP: [$OLDIP]" >> $LOG_PATH/dynhost.log
+    echo -n "New IP: [$IP]" >> $LOG_PATH/dynhost.log
+    RESULT=`wget -q -O - 'http://www.ovh.com/nic/update?system=dyndns&hostname='$HOST'&myip='$IP --user=$LOGIN --password=$PASSWORD >> $LOG_PATH/dynhost.log`
+    echo "Result: $RESULT" >> $LOG_PATH/dynhost.log
+    if [[ $RESULT =~ ^(good|nochg).* ]]; then
+      echo ---------------------------------- >> $LOG_PATH/dynhost-changes.log
+      echo `date` >> $LOG_PATH/dynhost-changes.log
+      echo "New IP : $IP" >> $LOG_PATH/dynhost-changes.log
+    fi
+  else
+    echo "Notice: IP $HOST [$OLDIP] is identical to WAN [$IP]! No update required." >> $LOG_PATH/dynhost.log
+  fi
+else
+  echo "Error: WAN IP not found. Exiting!" >> $LOG_PATH/dynhost.log
+fi
